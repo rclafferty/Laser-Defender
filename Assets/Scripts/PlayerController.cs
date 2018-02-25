cf26387a655cdc21125c80e3ec5e92fbbdc57fd3
Robert Lafferty <rclafferty@k-state.edu>
2018-02-25 17:16:54 -0600

    First Stretch of Progress
    Worked through the first 7 tutorials

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private float speed;
+    private float horizontalInput;
+    private float padding;
+
+    private float xmin;
+    private float xmax;
+
+	// Use this for initialization
+	void Start () {
+        speed = 15f;
+        horizontalInput = 0;
+        padding = 1f;
+
+        //xmin = -14;
+        //xmax = 14;
+
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1f, 0f, distance));
+        xmin = leftmost.x + padding;
+        xmax = rightmost.x - padding;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        horizontalInput = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(horizontalInput) > 0)
+        {
+            transform.position += new Vector3(horizontalInput * speed * Time.deltaTime, 0f, 0f);
+        }
+
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+}
