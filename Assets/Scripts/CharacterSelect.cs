68b55a5c555c7f2a16d082821ff739d6e88d44bd
Robert Lafferty <rclafferty@k-state.edu>
2018-02-25 23:24:39 -0600

    Added Character Select Screen
    Added a character select screen that allows the player to select which sprite they would like for their character.

---
+++ Assets/Scripts/CharacterSelect.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelect : MonoBehaviour {
+
+    GameObject[] characters;
+    int[] characterIndexes;
+
+    int selectedIndex;
+    int right1Index;
+    int right2Index;
+    int left1Index;
+    int left2Index;
+
+    Sprite[] characterSprites;
+
+	// Use this for initialization
+	void Start () {
+        
+        characters = new GameObject[5];
+        characters[0] = GameObject.Find("Left 2");
+        characters[1] = GameObject.Find("Left 1");
+        characters[2] = GameObject.Find("Selected Ship");
+        characters[3] = GameObject.Find("Right 1");
+        characters[4] = GameObject.Find("Right 2");
+
+        characterSprites = Resources.LoadAll<Sprite>("ShipSprites/Player");
+
+        characterIndexes = new int[5];
+        characterIndexes[0] = characterSprites.Length - 2;
+        characterIndexes[1] = characterSprites.Length - 1;
+        characterIndexes[2] = 0;
+        characterIndexes[3] = 1;
+        characterIndexes[4] = 2;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChooseLeft()
+    {
+        for (int i = 0; i < characterIndexes.Length; i++)
+        {
+            characterIndexes[i]--;
+            if (characterIndexes[i] == -1)
+            {
+                characterIndexes[i] = characterSprites.Length - 1;
+            }
+
+            characters[i].GetComponent<SpriteRenderer>().sprite = characterSprites[characterIndexes[i]];
+        }
+    }
+
+    public void ChooseRight()
+    {
+        for (int i = 0; i < characterIndexes.Length; i++)
+        {
+            characterIndexes[i]++;
+            if (characterIndexes[i] == characterSprites.Length)
+            {
+                characterIndexes[i] = 0;
+            }
+
+            characters[i].GetComponent<SpriteRenderer>().sprite = characterSprites[characterIndexes[i]];
+        }
+    }
+
+    public void SelectSpaceship()
+    {
+        GameObject player = GameObject.Find("Player");
+        player.GetComponent<SpriteRenderer>().sprite = characterSprites[characterIndexes[2]];
+        player.GetComponent<PlayerController>().SetActive(true);
+        SceneManager.LoadScene("Game");
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,38 +4,90 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    private static PlayerController thisPlayer = null;
+
     private float speed;
     private float horizontalInput;
+    private float verticalInput;
     private float padding;
 
     private float xmin;
     private float xmax;
+    private float ymin;
+    private float ymax;
 
-	// Use this for initialization
-	void Start () {
+    private bool isActive;
+
+    private void Awake()
+    {
+        if (thisPlayer == null)
+        {
+            thisPlayer = this;
+            DontDestroyOnLoad(this);
+            SetActive(false);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
         speed = 15f;
         horizontalInput = 0;
+        verticalInput = 0;
         padding = 1f;
-
-        //xmin = -14;
-        //xmax = 14;
-
+        
         float distance = transform.position.z - Camera.main.transform.position.z;
         Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, distance));
         Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1f, 0f, distance));
+        Vector3 upmost = Camera.main.ViewportToWorldPoint(new Vector3(0f, 1f, distance));
+        Vector3 downmost = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, distance));
+
+        Debug.Log("upmost: " + upmost);
+        Debug.Log("downmost: " + downmost);
+        Debug.Log("leftmost: " + leftmost);
+        Debug.Log("rightmost: " + rightmost);
         xmin = leftmost.x + padding;
         xmax = rightmost.x - padding;
+        ymin = downmost.y + padding;
+        ymax = upmost.y - padding;
     }
 	
 	// Update is called once per frame
-	void Update () {
-        horizontalInput = Input.GetAxis("Horizontal");
-        if (Mathf.Abs(horizontalInput) > 0)
+	void Update ()
+    {
+        if (isActive)
         {
-            transform.position += new Vector3(horizontalInput * speed * Time.deltaTime, 0f, 0f);
-        }
+            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
+            if (Mathf.Abs(horizontalInput) > 0 || Mathf.Abs(verticalInput) > 0)
+            {
+                transform.position += new Vector3(horizontalInput * speed * Time.deltaTime, verticalInput * speed * Time.deltaTime, 0f);
+            }
 
-        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
-        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+            float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+            float newY = Mathf.Clamp(transform.position.y, ymin, ymax);
+            transform.position = new Vector3(newX, newY, transform.position.z);
+        }
 	}
+
+    public void SetActive(bool tf)
+    {
+        if (tf)
+        {
+            Debug.Log("Set active");
+            isActive = true;
+            transform.position = new Vector3(0, -4f, 0);
+        }
+        else
+        {
+            Debug.Log("Set INACTIVE");
+            // Hide from camera
+            transform.position = new Vector3(0, -200f, 0);
+            isActive = false;
+        }
+    }
 }
